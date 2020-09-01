d674f851cb9205a20ce6685704b6b4566486a348
turlarion <aturluk5@bk.ru>
2020-09-01 21:32:57 +0300

    #12 Отображаются изображения и по нажатию на статью открывается фрагмент с самой статьей и фото

--- feature_dashboard/src/main/java/ru/mpei/feature_dashboard/DashboardFragment.kt
+++ feature_dashboard/src/main/java/ru/mpei/feature_dashboard/DashboardFragment.kt
@@ -2,10 +2,15 @@ package ru.mpei.feature_dashboard
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kekmech.ru.common_adapter.BaseAdapter
 import kekmech.ru.common_mvi.ui.BaseFragment
-import kotlinx.android.synthetic.main.fragment_news.*
+import kekmech.ru.common_navigation.NavigationHolder
+import kekmech.ru.common_navigation.ReplaceScreen
+import kekmech.ru.common_navigation.Router
+import kotlinx.android.synthetic.main.fragment_list.*
 import org.koin.android.ext.android.inject
 import ru.mpei.feature_dashboard.items.NewsAdapterItem
 import ru.mpei.feature_dashboard.mvi.*
@@ -14,10 +19,11 @@ import ru.mpei.feature_dashboard.mvi.DashboardEvent.Wish
 class DashboardFragment : BaseFragment<DashboardEvent, DashboardEffect, DashboardState, DashboardFeature>() {
     override val initEvent: DashboardEvent get() = Wish.System.Init
 
+    private val router: Router by inject()
     private val dashboardFeatureFactory: DashboardFeatureFactory by inject()
     override fun createFeature(): DashboardFeature = dashboardFeatureFactory.create()
 
-    override var layoutId = R.layout.fragment_news
+    override var layoutId = R.layout.fragment_list
 
     private lateinit var adapter: BaseAdapter
 
@@ -36,8 +42,15 @@ class DashboardFragment : BaseFragment<DashboardEvent, DashboardEffect, Dashboar
     }
 
     private fun createAdapter() = BaseAdapter(
-        NewsAdapterItem { newsItem ->
-            println("You clicked on $newsItem")
+        NewsAdapterItem {
+            val bundle = Bundle()
+            bundle.putString("head", it.name)
+            bundle.putString("date", it.hour + " " + it.chislo + " " + it.month)
+            bundle.putString("content", it.content)
+            bundle.putString("imageUrl", "http://cy37212.tmweb.ru/images/news/" + it.imageUrl)
+            val fragment = ArticleFragment()
+            fragment.arguments = bundle
+            router.executeCommand(ReplaceScreen { fragment } )
         }
     )
 }
--- feature_dashboard/src/main/java/ru/mpei/feature_dashboard/items/DashboardItem.kt
+++ feature_dashboard/src/main/java/ru/mpei/feature_dashboard/items/DashboardItem.kt
@@ -1,7 +1,6 @@
 package ru.mpei.feature_dashboard.items
 
 import android.view.View
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kekmech.ru.common_adapter.AdapterItem
 import kekmech.ru.common_adapter.BaseItemBinder
@@ -9,43 +8,24 @@ import kotlinx.android.extensions.LayoutContainer
 import ru.mpei.domain_news.dto.NewsItem
 import ru.mpei.feature_dashboard.R
 
-interface NewsViewHolder {
-    fun setName(name: String)
-    fun setDate(chislo: String, month: String, hour: String)
-    fun setOnClickListener(onClick: () -> Unit)
+interface DashboardViewHolder {
 }
 
-class NewsViewHolderImpl(
-        override val containerView: View
-) : RecyclerView.ViewHolder(containerView), NewsViewHolder, LayoutContainer {
+class DashboardViewHolderImpl(
+    override val containerView: View
+): RecyclerView.ViewHolder(containerView), DashboardViewHolder, LayoutContainer {
 
-    override fun setDate(chislo: String, month: String, hour: String) {
-        // todo
-    }
+}
 
-    override fun setName(name: String) {
-        containerView.findViewById<TextView>(R.id.articleElementText).setText(name)
-    }
+class AdapterItemBinder(): BaseItemBinder<DashboardViewHolder, NewsItem>() {
 
-    override fun setOnClickListener(onClick: () -> Unit) {
-        containerView.setOnClickListener { onClick() }
+    override fun bind(vh: DashboardViewHolder, model: NewsItem, position: Int) {
     }
 }
 
-class NewsItemBinder(
-    private val onClick: (NewsItem) -> Unit
-) : BaseItemBinder<NewsViewHolder, NewsItem>() {
-
-    override fun bind(vh: NewsViewHolder, model: NewsItem, position: Int) {
-        vh.setDate(model.chislo, model.month, model.hour)
-        vh.setName(model.name)
-        vh.setOnClickListener { onClick(model) }
-    }
-}
-
-class NewsAdapterItem(onClick: (NewsItem) -> Unit) : AdapterItem<NewsViewHolder, NewsItem>(
-    isType = { it is NewsItem },
-    layoutRes = R.layout.item_article,
-    itemBinder = NewsItemBinder(onClick),
-    viewHolderGenerator = ::NewsViewHolderImpl
+class DashboardAdapterItem: AdapterItem<DashboardViewHolder, NewsItem>(
+        isType = { it is NewsItem },
+        layoutRes = R.layout.fragment_list,
+        itemBinder = AdapterItemBinder(),
+        viewHolderGenerator = ::DashboardViewHolderImpl
 )

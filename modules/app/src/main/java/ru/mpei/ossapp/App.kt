0c101b3b166b8073006768638895edf1f01f07d9
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2021-03-11 21:55:05 +0300

    Remove unused code

--- modules/app/src/main/java/ru/mpei/ossapp/App.kt
+++ modules/app/src/main/java/ru/mpei/ossapp/App.kt
@@ -1,8 +1,6 @@
 package ru.mpei.ossapp
 
 import android.app.Application
-import android.content.Context
-import android.content.SharedPreferences
 import kekmech.ru.common_di.modules
 import kekmech.ru.common_navigation.di.NavigationModule
 import kekmech.ru.common_network.di.NetworkModule
@@ -13,7 +11,6 @@ import ru.mpei.feature_tasks.di.TasksModule
 import ru.mpei.feature_dashboard.di.DashboardModule
 import ru.mpei.feature_profile.di.ProfileModule
 import ru.mpei.feature_services.di.ServicesModule
-import ru.mpei.ossapp.activities.MainActivity.Companion.APP_PREFERENCES
 import ru.mpei.ossapp.di.AppModule
 import ru.mpei.ossapp.ui.main.di.MainScreenModule
 import timber.log.Timber
--- modules/app/src/main/java/ru/mpei/ossapp/pojo/Profile.kt
+++
@@ -1,9 +0,0 @@
-package ru.mpei.ossapp.pojo
-
-data class Profile(
-    var id: Int = 0,
-    var name: String = "",
-    var surname: String = "",
-    var email: String = "",
-    var capital: Int = 0
-)

980b6d1fd5d31b03793fc88e39c266b25e90163f
aleksandrfofanov <mister.fo1996@gmail.com>
2022-03-21 16:04:58 +0300

    Начал переход на бд в отправителе уведомлений

--- domain_timetable/src/main/java/ru/acediat/domain_timetable/TimetableApi.kt
+++ domain_timetable/src/main/java/ru/acediat/domain_timetable/TimetableApi.kt
@@ -1,4 +1,30 @@
 package ru.acediat.domain_timetable
 
+import io.reactivex.Observable
+import kekmech.ru.common_annotations.EndpointUrl
+import kekmech.ru.common_kotlin.OSS_URL
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+//@EndpointUrl("http://ruz.public.mpei.local/RUZService/ruzservice.svc/")
+@EndpointUrl(OSS_URL)
 interface TimetableApi {
+
+    @GET("lk/shedule.php")
+    fun getPersonLessons(
+        @Query("fromdate") fromDate : String,
+        @Query("todate") toDate : String,
+        @Query("groupoid") groupOId : Int
+    ) : Observable<List<LessonsDTO>>
+
+    companion object{
+        val RECEIVER_TYPE_STUDENT = 0
+        val RECEIVER_TYPE_TEACHER = 1
+        val RECEIVER_TYPE_ROOM = 2
+        val RECEIVER_TYPE_GROUP = 3
+
+        val LANGUAGE_RUSSIAN = 1
+        val LANGUAGE_ENGLISH = 2
+    }
 }
--- service_notifications/src/main/java/ru/mpei/service_notifications/NotificationSender.kt
+++ service_notifications/src/main/java/ru/mpei/service_notifications/NotificationSender.kt
@@ -9,24 +9,43 @@ import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationManagerCompat
-import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.Flowable
+import io.reactivex.Single
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import kekmech.ru.common_kotlin.OSS_URL
 import kekmech.ru.common_network.retrofit.buildApi
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import ru.mpei.domain_tasks.AVAILABLE
+import ru.mpei.domain_tasks.TasksDatabaseRepository
 import ru.mpei.domain_tasks.TasksRepository
 import ru.mpei.domain_tasks.dto.TasksItem
 import java.util.concurrent.TimeUnit
 
+@SuppressLint("CheckResult")
 @RequiresApi(Build.VERSION_CODES.O)
 class NotificationSender(private val context : Context) {
 
-    private val SENDER_LOG = "sender_log"
+    private val SENDER_LOG = "OSS_TAG_SENDER"
 
     var enabled = false
         private set
+
+    private var notificationId = 0
+    private val manager = NotificationManagerCompat.from(context)
+    private var tasks = ArrayList<TasksItem>()
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private val channel = NotificationChannel(
+        "oss channel",
+        "oss",
+        NotificationManager.IMPORTANCE_DEFAULT
+    )
+
+    private val userId = context.getSharedPreferences("settings", Context.MODE_PRIVATE)
+        .getString("userId", "")!!
 
     private var tasksRepository = TasksRepository(
         Retrofit.Builder()
@@ -35,63 +54,47 @@ class NotificationSender(private val context : Context) {
             .addConverterFactory(GsonConverterFactory.create())
             .buildApi()
     )
+    private val databaseRepository = TasksDatabaseRepository(context)
 
-    private var notificationId = 0
-    private val manager = NotificationManagerCompat.from(context)
-    @RequiresApi(Build.VERSION_CODES.O)
-    private val channel = NotificationChannel("oss channel", "oss", NotificationManager.IMPORTANCE_DEFAULT)
-
-    private var tasksList : ArrayList<TasksItem> = arrayListOf()
+    private lateinit var tasksObserver : Disposable
 
     init{
-        Log.d(SENDER_LOG, "init notification service...")
+        Log.d(SENDER_LOG, "init notification sender...")
         channel.description = "descr"
         manager.createNotificationChannel(channel)
-        tasksRepository.observeTasks(
-            context.getSharedPreferences("settings", Context.MODE_PRIVATE)
-                .getString("userId", "")!!, "available")
-            .subscribeOn(Schedulers.io())
-            .observeOn(Schedulers.computation())
-            .doOnSuccess {
-                tasksList = it as ArrayList<TasksItem>
-            }.doOnError {
-
-            }.subscribe()
-        logTasks("init tasks: ", tasksList)
     }
 
+    //TODO: Добавить работу с бд, потому что сейчас tasks всегда будет пустой
     fun startSending(){
         enabled = true
-        doWithTasks(
-            {gotTasks ->
-                logTasks("received tasks:", gotTasks)
-                var notifyList = gotTasks.filter{ !tasksList.contains(it) }
-                logTasks("notify list: ", notifyList)
-                for(t in notifyList){
-                    sendNotification(TaskNotification(t).getNotification(context))
-                    notifyList = notifyList.filter { it != t }
-                    tasksList.add(t)
-                }
-            },
-            {
-                Log.d(SENDER_LOG, "error!", it)
-                this.startSending()
-            },
-            {Log.d(SENDER_LOG, "succeed!")})
+        tasksObserver = repeatWithTasks(10).subscribe({
+            Log.d(SENDER_LOG, "Checking...")
+            val notifyTasks = it.filter { !tasks.contains(it) }
+            for (task in notifyTasks) {
+                sendNotification(TaskNotification(task).getNotification(context))
+            }
+        }, {
+            Log.e(SENDER_LOG, "ERROR!", it)
+        })
     }
 
-    @SuppressLint("CheckResult")
-    private fun doWithTasks(onNext : (gotTasks : List<TasksItem>) -> Unit,
-                            onError : (it : Throwable) -> Unit,
-                            onComplete : () -> Unit){
-        tasksRepository.observeTasks(
-            context.getSharedPreferences("settings", Context.MODE_PRIVATE)
-                .getString("userId", "")!!, "available")
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .repeatWhen { t -> t.delay(10, TimeUnit.SECONDS) }
-            .subscribe({onNext(it)}, {onError(it)}, {onComplete()})
+    fun stopSending(){
+        enabled = false
+        tasksObserver.dispose()
     }
+
+    private fun repeatWithTasks(repeatTime : Long) : Flowable<List<TasksItem>> =
+        getNewTasks().repeatWhen { it.delay(repeatTime, TimeUnit.SECONDS) }
+
+    private fun getNewTasks() : Single<List<TasksItem>> =
+        tasksRepository.observeTasks(userId, AVAILABLE)
+        .subscribeOn(Schedulers.io())
+        .observeOn(Schedulers.computation())
+
+    private fun getLocalTasks() = databaseRepository.getAllTasks()
+        .subscribeOn(Schedulers.io())
+        .observeOn(Schedulers.io())
+        .subscribe{ tasks = it as ArrayList<TasksItem> }
 
     private fun sendNotification(notification : Notification) {
         Log.d(SENDER_LOG, "Sending notification...")
